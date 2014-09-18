0bcf99dea95e5222f48892ab0f78686a56104386
Dilts <brianedilts@gmail.com>
2014-09-18 11:37:38 -0600

    changed index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
-var userModel = require('../models/users.js')
+var mongoose = require('mongoose');
+var userModel = require('../models/users.js');
 
 var indexController = {
 	index: function(req, res) {
@@ -6,20 +7,26 @@ var indexController = {
 	},
 
 	createUpload: function(req, res) {
-    console.log(req.body);
 
-    req.user.name = req.body.name
-    req.user.url = req.body.url
-    req.user.title = req.body.title
-    req.user.desc = req.files.desc
+    var user = new userModel({
+	    name: req.body.name,
+	    url: req.body.url,
+	    title: req.body.title,
+	    desc: req.body.desc,
+	    });  	
+    
+    // req.user.name = req.body.name,
+    // req.user.url = req.body.url,
+    // req.user.title = req.body.title,
+    // req.user.desc = req.body.desc,
 
-    console.log(req.user)
-    req.user.save(function(err, doc) {
+    console.log(user)
+
+    user.save( function(err, doc) {
     	if (err) {
     		console.log('error not saving', err)
-    		res.send(500, 'did not save')
+    		res.send(status, 'did not save')
     	}
-
     }) 
 
     res.render('index', req)
